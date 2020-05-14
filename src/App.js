c5c1bf772c7c9fdc91ab23319fb60bc6b5f1995f
Jae Young Choi <jaeyoung@Jaes-MBP.fios-router.home>
2020-05-14 14:50:20 -0400

    functionality down

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import image from './images/download.png';
+
 class App extends React.Component {
-  state = { data: {} };
+  state = { data: {}, country: '' };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
-        <Cards data={this.state.data} />
-        <CountryPicker />
-        <Chart />
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
