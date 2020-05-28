29bace6e2d74c5cde5ebb6d0d0e84aeabb068162
jaeyoung <jaeyoungnsb@gmail.com>
2020-05-27 22:18:23 -0400

    Updates

---
+++ precache-manifest.8b2c88cb77e0bb0d703eb7eea9829190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0d494f5456b386c3e26bcb60e7593e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "faa69843a55fbbb67957",
+    "url": "/covid-19-tracker/static/css/main.8beb0c25.chunk.css"
+  },
+  {
+    "revision": "5a192b92e0eba46b819d",
+    "url": "/covid-19-tracker/static/js/2.d45a3fb0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.d45a3fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa69843a55fbbb67957",
+    "url": "/covid-19-tracker/static/js/main.f7b821a8.chunk.js"
+  },
+  {
+    "revision": "50404f43e9c58a12ef74",
+    "url": "/covid-19-tracker/static/js/runtime-main.ea81220e.js"
+  }
+]);
